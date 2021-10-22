b2c18842f7fe78333cb41ed33a2c0f1dd07c448d
Silke Meyer <silke.meyer@dmx.berlin>
2021-10-22 10:32:41 +0200

    Removed the articles from the strings

--- src/main/js/lang-strings.js
+++ src/main/js/lang-strings.js
@@ -2,9 +2,9 @@ export default {
   'header.shared_workspace.de': 'Gemeinsamer Arbeitsbereich',
   'header.shared_workspace.fr': 'Espace de travail commun',
   'add.note.de': 'Notiz',
-  'add.note.fr': 'La note',
+  'add.note.fr': 'Note',
   'add.document.de': 'Dokument',
-  'add.document.fr': 'Le document',
+  'add.document.fr': 'Document',
   'add.textfield.de': 'Textfeld',
-  'add.textfield.fr': 'La description',
+  'add.textfield.fr': 'Description',
 }

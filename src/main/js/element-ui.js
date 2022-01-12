cb90bb6ef993a9200851f0286b41c8ffc395ce94
Jörg Richter <jri@deepamehta.de>
2022-01-12 20:38:39 +0100

    Fix: add missing components for login dialog
    See dmx-projects/zukunftswerk#7
    See dmx-projects/zukunftswerk#21

--- src/main/js/element-ui.js
+++ src/main/js/element-ui.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import {Button, Input, Dropdown, DropdownMenu, DropdownItem, Dialog, Upload, Loading} from 'element-ui'
+import {Button, Input, Select, Option, Dropdown, DropdownMenu, DropdownItem, Dialog, Upload, Loading} from 'element-ui'
 import locale from 'element-ui/lib/locale'
 import DialogDraggable from 'vue-element-dialog-draggable'
 
@@ -9,6 +9,8 @@ locale.use(require('element-ui/lib/locale/lang/en').default)
 // register components
 Vue.use(Button)
 Vue.use(Input)
+Vue.use(Select)
+Vue.use(Option)
 Vue.use(Dropdown)
 Vue.use(DropdownMenu)
 Vue.use(DropdownItem)

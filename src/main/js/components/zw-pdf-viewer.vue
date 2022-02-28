772b3daeabcdb7e377773dea5f0533a3f1082e40
Jörg Richter <jri@deepamehta.de>
2022-02-28 23:58:59 +0100

    PDF viewer: add page browser
    See dmx-projects/zukunftswerk#33

--- src/main/js/components/zw-pdf-viewer.vue
+++ src/main/js/components/zw-pdf-viewer.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="zw-pdf-viewer">
     <canvas ref="canvas"></canvas>
+    <div class="toolbar">
+      <el-button type="text" icon="el-icon-arrow-left" @click="prev"></el-button>
+      <el-input class="page" v-model="pageNr"></el-input> / {{numPages}}
+      <el-button type="text" icon="el-icon-arrow-right" @click="next"></el-button>
+    </div>
   </div>
 </template>
 
@@ -10,29 +15,68 @@ pdfjs.GlobalWorkerOptions.workerSrc = '/systems.dmx.zukunftswerk/js/pdf.worker.j
 
 export default {
 
+  props: {
+    src: {
+      type: String,
+      required: true
+    }
+  },
+
+  data () {
+    return {
+      pdf: undefined,
+      pageNr: 1
+    }
+  },
+
+  computed: {
+    numPages () {
+      return this.pdf?.numPages
+    }
+  },
+
   created () {
     this.$emit('loading')
     pdfjs.getDocument(this.src).promise.then(pdf => {
-      return pdf.getPage(1)
-    }).then(page => {
-      const scale = 1.5
-      const viewport = page.getViewport({scale})
-      const canvas = this.$refs.canvas
-      canvas.width = viewport.width
-      canvas.height = viewport.height
-      return page.render({
-        canvasContext: canvas.getContext('2d'),
-        viewport
-      }).promise
+      this.pdf = pdf
+      return this.renderPage()
     }).then(() => {
       this.$emit('complete')
     })
   },
 
-  props: {
-    src: {
-      type: String,
-      required: true
+  watch: {
+    pageNr () {
+      this.renderPage()
+    }
+  },
+
+  methods: {
+
+    renderPage () {
+      this.pdf.getPage(this.pageNr).then(page => {
+        const scale = 1.5
+        const viewport = page.getViewport({scale})
+        const canvas = this.$refs.canvas
+        canvas.width = viewport.width
+        canvas.height = viewport.height
+        return page.render({
+          canvasContext: canvas.getContext('2d'),
+          viewport
+        }).promise
+      })
+    },
+
+    prev () {
+      if (this.pageNr > 1) {
+        this.pageNr--
+      }
+    },
+
+    next () {
+      if (this.pageNr < this.numPages) {
+        this.pageNr++
+      }
     }
   }
 }
@@ -41,5 +85,9 @@ export default {
 <style>
 .zw-pdf-viewer canvas {
   width: 100%;
+}
+
+.zw-pdf-viewer .toolbar .page {
+  width: 50px;
 }
 </style>

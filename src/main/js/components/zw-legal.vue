a22968ac83ad710f8a898e21aadc636d0c9064b9
Jörg Richter <jri@deepamehta.de>
2022-04-02 20:23:30 +0200

    Style legal pages
    See dmx-projects/zukunftswerk#44

--- src/main/js/components/zw-legal.vue
+++ src/main/js/components/zw-legal.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="zw-legal">
-    <img class="logo" :src="logoSrc">
-    <zw-language-switch></zw-language-switch>
-    <h3><zw-string>{{heading}}</zw-string></h3>
+    <div class="header">
+      <img :src="logoSrc">
+      <zw-language-switch></zw-language-switch>
+    </div>
+    <div>
+      <div class="heading">
+        <zw-string>{{heading}}</zw-string>
+      </div>
+      <div class="text">
+        <zw-string>{{text}}</zw-string>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -16,7 +25,15 @@ export default {
   computed: {
 
     heading () {
-      return 'label.' + this.router.currentRoute.name
+      return 'label.' + this.routeName
+    },
+
+    text () {
+      return this.routeName
+    },
+
+    routeName () {
+      return this.router.currentRoute.name
     },
 
     router () {
@@ -28,5 +45,34 @@ export default {
 
 <style>
 .zw-legal {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  box-sizing: border-box;
+  background-color: black;
+  background-image: url("../../resources/zw-snake.png");
+  background-position: bottom right;
+  background-repeat: no-repeat;
+  padding: 60px 0 10px 160px;
+}
+
+.zw-legal .header {
+  margin-left: -81px;
+}
+
+.zw-legal .header img {
+  vertical-align: top;
+  margin-right: 180px;
+}
+
+.zw-legal .heading {
+  color: var(--primary-color);
+  font-size: 20px;
+  margin-top: 42px;
+}
+
+.zw-legal .text {
+  color: white;
+  margin-top: 16px;
 }
 </style>

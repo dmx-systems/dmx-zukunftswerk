d7d2443d62df3ff12e4c32233fbd2fa8b23f4137
Jörg Richter <jri@deepamehta.de>
2022-04-10 15:58:31 +0200

    Make use of new dmx-api `topicTypes` config option
    See dmx-platform/dmx-platform#501

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -8,29 +8,18 @@ import onHttpError from './error-handler'
 import messageHandler from './message-handler'
 import './element-ui'
 
-console.log('[ZW] 2022/04/09-2')
+console.log('[ZW] 2022/04/10')
 
-// 1) Init dmx library ### TODO: only top-level types should be required here
-dmx.init({store, onHttpError})
-dmx.typeCache.initTopicType('zukunftswerk.document')
-dmx.typeCache.initTopicType('zukunftswerk.document_name.de')
-dmx.typeCache.initTopicType('zukunftswerk.document_name.fr')
-dmx.typeCache.initTopicType('zukunftswerk.note')
-dmx.typeCache.initTopicType('zukunftswerk.note.de')
-dmx.typeCache.initTopicType('zukunftswerk.note.fr')
-dmx.typeCache.initTopicType('zukunftswerk.label')
-dmx.typeCache.initTopicType('zukunftswerk.label.de')
-dmx.typeCache.initTopicType('zukunftswerk.label.fr')
-dmx.typeCache.initTopicType('zukunftswerk.language')
-dmx.typeCache.initAssocType('zukunftswerk.original_language')
-dmx.typeCache.initAssocType('zukunftswerk.de')
-dmx.typeCache.initAssocType('zukunftswerk.fr')
-dmx.typeCache.initTopicType('dmx.files.file')
-dmx.typeCache.initTopicType('dmx.files.file_name')
-dmx.typeCache.initTopicType('dmx.files.path')
-dmx.typeCache.initTopicType('dmx.files.media_type')
-dmx.typeCache.initTopicType('dmx.files.size')
-dmx.typeCache.initAssocType('dmx.core.composition')
+// 1) Init dmx library
+dmx.init({
+  topicTypes: [       // needed e.g. for form generator
+    'zukunftswerk.document',
+    'zukunftswerk.note',
+    'zukunftswerk.label'
+  ],
+  store,
+  onHttpError
+})
 
 // 2) Open websocket connection
 new DMXWebSocket('systems.dmx.webclient', messageHandler)

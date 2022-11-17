0d80b31cf8fbac40e4dd8183c9bad081deba3bc0
Jörg Richter <jri@deepamehta.de>
2022-11-17 15:14:21 +0100

    Data model: add "Textblock"
    See dmx-projects/zukunftswerk#168

--- src/main/java/systems/dmx/zukunftswerk/migrations/Migration8.java
+++ src/main/java/systems/dmx/zukunftswerk/migrations/Migration8.java
@@ -10,6 +10,7 @@ import systems.dmx.core.service.Migration;
 
 /**
  * Extends topic type "Document" by "Original Language".
+ * Note: before ZW 1.5 Document(Name)s were not auto-translated.
  * <p>
  * Part of Zukunftswerk 1.5
  * Runs ALWAYS.
@@ -24,5 +25,6 @@ public class Migration8 extends Migration {
             mf.newCompDefModel(ORIGINAL_LANGUAGE, false, false, DOCUMENT, LANGUAGE, ONE),
             FILE + "#" + DE
         );
+        // FIXME: add view config to comp def
     }
 }

74b5b1cb494b33b34f804f1ec23e7b2b66168c95
Jörg Richter <jri@deepamehta.de>
2023-05-04 17:30:46 +0200

    Migration 13: remove assignments from workspace assocs
    See dmx-platform/dmx-platform#507

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -7,7 +7,7 @@ import onHttpError from './error-handler'
 import messageHandler from './message-handler'
 import './element-ui'
 
-console.log('[ZW] 2023/05/02')
+console.log('[ZW] 2023/05/04')
 
 // 1) Init dmx library
 dmx.init({

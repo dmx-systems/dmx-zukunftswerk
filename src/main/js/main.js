a1dff10a6ef9df75098d622fac7d7e55cdc6fcab
Jörg Richter <jri@deepamehta.de>
2023-06-23 10:28:59 +0200

    Bump DMX dependencies to 5.3

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -7,7 +7,7 @@ import onHttpError from './error-handler'
 import messageHandler from './message-handler'
 import './element-ui'
 
-console.log('[ZW] 2023/06/10')
+console.log('[ZW] 2023/06/23')
 
 // 1) Init dmx library
 dmx.init({
